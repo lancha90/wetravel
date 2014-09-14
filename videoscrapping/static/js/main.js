26c1fbd67c430413c7a7a7e05a408bd809f38e00
diego mauricio herrera alzate <diegomao627@gmail.com>
2014-09-14 11:02:50 -0500

    implementación del filtro de busqueda y hover de los items

--- videoscrapping/static/js/main.js
+++ videoscrapping/static/js/main.js
@@ -1,22 +1,41 @@
+var search_video = function(){
+    search_text=$('.menu #input-search').val();
+    if(search_text.trim().length > 0 ){
+        $('div.item').fadeOut();
+        $( "div.item:contains('"+search_text+"')").fadeIn(function(){
+            $(window).trigger('scroll');
+        });
+    }
+};
+
 var init = function() {
 
     $('div.item').on('click',function(){
         location.href=$(this).attr('href');
     });
 
+    $('.menu #btn-search').on('click',search_video);
+    $('.menu #input-search').keypress(function(e) {
+        if(e.which == 13) {
+            search_video();
+        }
+    });
 
     $('ul#menu-channels li').on('click',function(){
         $('ul#menu-channels li').removeClass('active');
         $(this).addClass('active');
         ref=$(this).attr('ref');
+        //$('div.item[ref="'+ref+'"] img.lazy').slice(0,12).show().lazyload();
 
         if(ref!=0){
-
             $('div.item[ref="'+ref+'"]').fadeIn();
-            $('div.item[ref!="'+ref+'"]').fadeOut();
+            $('div.item[ref!="'+ref+'"]').fadeOut(function(){
+                $(window).trigger('scroll');
+            });
         }else{
             $('div.item').fadeIn();
-        }
+        }    
+
 
     });
 
