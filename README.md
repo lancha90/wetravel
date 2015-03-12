08249bb5b6afd217cfce28304abae6224b02d6da
diego mauricio herrera alzate <diegomao627@gmail.com>
2015-03-12 14:22:05 -0500

    modificación de interface web

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-wetravel
+WeTravel
 ========
 
-django cliente for channels from youtube
+Scrapping para canales de youtube desarrollado en Django Python
+
+http://wetravel.herokuapp.com/
+
--- videoscrapping/static/js/main.js
+++ videoscrapping/static/js/main.js
@@ -53,3 +53,18 @@ var init = function() {
 };
 
 $(document).on('ready',init);
+
+
+$(window).load(function(){
+    $('img.bgfade').hide();
+    var dg_H = $(window).height();
+    var dg_W = $(window).width();
+    $('#wrap').css({'height':dg_H,'width':dg_W});
+    function anim() {
+        $("#wrap img.bgfade").first().appendTo('#wrap').fadeOut(5000);
+        $("#wrap img").first().fadeIn(5000);
+        setTimeout(anim, 6000);
+    }
+    anim();});
+
+$(window).resize(function(){window.location.href=window.location.href})
